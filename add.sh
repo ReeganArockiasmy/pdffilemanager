185675d85c5d7c9f5c38ae6190077ead356b218d
your name <yor eMail adress>
2018-11-13 07:53:41 +0100

    validation added

--- add.sh
+++ add.sh
@@ -1,3 +1,56 @@
 #! /bin/bash
+jsonfile=pdffiledata.json
 
-x
+bookIsThere(){
+    if jshon -k < $jsonfile | grep -q $filename
+    then
+	return 1
+    fi
+}
+
+#for filepath in `find $HOME -name \*.pdf`
+for filepath in `find $PWD -name \*.pdf`
+do
+filename=$(basename "$filepath")
+dir=$(dirname "$filepath")"/"
+if bookIsThere
+then
+    echo $filename
+    select var in open tags noadd exit
+    do
+	case $var in
+	    open|1 )
+		evince $dir$filename
+		;;
+	    tags|2 )
+		read -p "tags: " tags
+		break
+		;;
+	    noadd|3 )
+		break
+		;;
+	    exit|4 )
+		exit 0
+		;;
+	    *)
+		echo "anything"
+		;;
+	esac 
+    done
+    
+    if [[ $var != "noadd" ]]
+    then
+	expect <<EOF
+set timeout -1
+spawn ./input.sh
+expect "name\r"
+send -- "$filename\r"
+expect "location\r"
+send -- "$dir\r"
+expect "tags\r"
+send -- "$tags\r"
+expect eof
+EOF
+    fi
+fi    
+done
