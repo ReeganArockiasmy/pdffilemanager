cd36b0b0463581bf725b116a4511cdb139f7cda5
your name <yor eMail adress>
2018-11-19 10:34:14 +0100

    print format line addes

--- printformat.sh
+++ printformat.sh
@@ -16,6 +16,20 @@ printbooks() {
     jshon -k < $database
 }
 
+printline() {
+    tag="tags"
+    printf "%0.s-" echo `seq 1 $padmax`
+    printf "\n"
+    printf "%s" "$tag"
+    printf '%*.*s' 0 $((padlength - ${#tag} )) "$pad"
+    printf "%s\n" "Book Name"
+    printf "%0.s-" echo `seq 1 $padmax`
+    printf "\n"
+    
+}
+
+
+
 # print all
 #https://stackoverflow.com/questions/4409399/padding-characters-in-printf
 printall() {
@@ -29,7 +43,8 @@ printall() {
     padmax=`jshon -k < $database | wc -L`
     padmax=$((padlength+padmax))
     echo $padmax
-    pad=$(printf '%0.1s' " "{1..42})
+    pad=$(printf '%0.1s' " "{1..42}) # replace padmax in 42
+    printline
     for name in `jshon -k < pdffiledata.json`
     do
 	
