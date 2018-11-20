6ec39adc7dee0ed43a1037eb769858f267b389cb
your name <yor eMail adress>
2018-11-20 14:05:57 +0100

    modification database will improve

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # check json file is empty
 jsonfile="pdffiledata.json"
-
+database="pdffiledata.json"
 # It is create a file , is not exist
 if [ ! -f $jsonfile ]
 then
@@ -20,6 +20,44 @@ get_id() {
     fi
     return $id
 }
+
+# . ./delete.sh --source-only # is is add delete source code
+
+improve() {
+    echo "Enter the file no"
+    select var in `jshon -k <$database`
+    do
+	echo $var
+	select opt in view modify quit
+	do
+	    case $opt in
+		view|1 )
+		    result=`jshon -e $var < $database`
+		    location=`jshon -e $var -e location < $database`
+		    location=$(echo $location | sed -e 's/\\//g' -e 's/^.\(.*\).$/\1/')
+		    tags=`jshon -e $var -e tag < $database`
+		    echo "Name :" $var
+		    echo "location :" $location
+		    echo "tags :" $tags
+		    ;;
+		modify|2 )
+		    read -p "tags: " tags
+		    eval jshon -e $var -s $tags -i tag -I -F $database < $database
+		    exit
+		    ;;
+		quit|3 )
+		    exit
+		    ;;
+		esac
+	  done
+	
+    done
+    
+	
+}
+
+
+
 
 
 # it call the fuction in commandline in direct
